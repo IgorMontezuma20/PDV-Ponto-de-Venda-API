a52894f075234f3be5696d136316c49c8a71ea13
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-25 20:52:12 -0300

    feat: criando os repositories para as entidades

---
+++ src/main/java/com/example/pdv/repository/SaleItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.pdv.repository;
+
+import com.example.pdv.entity.SaleItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleItemRepository extends JpaRepository<SaleItem, Long> {
+}
---
+++ src/main/java/com/example/pdv/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.pdv.repository;
+
+import com.example.pdv.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
