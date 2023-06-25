905175036ec2edcdb83ccf8b7b9ddab69e733108
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-25 19:45:33 -0300

    feat: create product entity

---
+++ src/main/java/com/example/pdv/entity/Product.java
@@ -0,0 +1,31 @@
+package com.example.pdv.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "product")
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String description;
+
+    @Column(length = 20, precision = 20, scale = 2, nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+}
--- src/main/java/com/example/pdv/entity/User.java
+++ src/main/java/com/example/pdv/entity/User.java
@@ -1,13 +1,19 @@
 package com.example.pdv.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Table(name = "user")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100, nullable = false)
     private String name;
     private boolean isEnable;
 
