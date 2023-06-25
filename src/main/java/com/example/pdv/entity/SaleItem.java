094d85401e5b018e9fa0f77a7eb9194a37c72f9e
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-25 20:04:06 -0300

    feat: criando entidades Sale e SaleItem e implementando o relacionamento entre elas

--- src/main/java/com/example/pdv/entity/SaleItem.java
+++ src/main/java/com/example/pdv/entity/SaleItem.java
@@ -5,25 +5,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(name = "product")
+@Table(name = "sale_item")
 @Entity
-public class Product {
+public class SaleItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 100, nullable = false)
-    private String description;
-
-    @Column(length = 20, precision = 20, scale = 2, nullable = false)
-    private BigDecimal price;
-
     @Column(nullable = false)
     private int quantity;
 
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id", nullable = false)
+    private Sale sale;
 }
